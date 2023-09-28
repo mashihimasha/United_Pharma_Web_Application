544301240e0863bbeb161ffc0657af7ae2a1b8ae
Minoli Fonseka <mashihimasha@gmail.com>
2023-09-28 23:24:57 +0530

    deleted login components

--- Front-End/united-pharma-web-app/src/App.js
+++ Front-End/united-pharma-web-app/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to My App</h1>
-      <LoginPage />
       {/* Other components or content */}
     </div>
   );
