6f7e4b9efb82cdcb1fc1a0d84d08bd9196e46fe4
iresha-perera <madushanii619@gmail.com>
2023-10-06 15:09:14 +0530

    modified:   Front-End/united-pharma-web-app/src/components/catalog/WishlistBtn.js

--- Front-End/united-pharma-web-app/src/components/catalog/WishlistBtn.js
+++ Front-End/united-pharma-web-app/src/components/catalog/WishlistBtn.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class WishlistBtn extends Component {
   render() {
     return (
       <a href="#" className="cart">
-        <i className="fa-eye p-3"></i>
-        <FontAwesomeIcon icon="fa-regular fa-coffee" />
+        <i className=""></i>
       </a>
     );
   }
