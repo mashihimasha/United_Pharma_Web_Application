cfc44c251b1865a07766be3c667420fafbbb3eb4
Iresha Madushani <madushanii619@gmail.com>
2023-10-13 23:11:32 +0530

    add product catalog

---
+++ Front-End/united-pharma-web-app/src/components/catalog/ProductName.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ProductName extends Component {
+  render() {
+    return (
+        <div className="title">
+            <h4>
+                <a href="shop-details.html">Box Full of Muscles</a>
+            </h4>
+            <span className="home-shop-price">Rs.3000.00</span>
+        </div>
+      
+    );
+  }
+}
+
+export default ProductName;
--- Front-End/united-pharma-web-app/src/pages/ProductCatalog.js
+++ Front-End/united-pharma-web-app/src/pages/ProductCatalog.js
@@ -3,32 +3,28 @@ import '../components/assets/css/catalog.css';
 import AddCartBtn from '../components/catalog/AddCartBtn';
 import WishlistBtn from '../components/catalog/WishlistBtn';
 import Rating from '../components/catalog/Rating';
+import ProductName from '../components/catalog/ProductName';
+import Category from '../components/catalog/Category';
+import ProductThumbnail from '../components/catalog/Thumbnail';
 
 class ShopItem extends Component {
   render() {
     return (
+      <div class="card p-3 m-2 ms-5" style={{ width: '18rem' }}>
       <div className="col-xl-4 col-lg-6 col-md-6">
         <div className="home-shop-item inner-shop-item">
-          <div className="home-shop-thumb">
-            <a href="shop-details.html">
-              <img src="assets/img/products/home_shop_thumb01.png" alt="img" />
-              <span className="discount"> -15%</span>
-            </a>
-          </div>
+          <div className="home-shop-thumb"><ProductThumbnail/></div>
           <div className="home-shop-content">
-            <div className="shop-item-cat"><a href="#">Nutrition</a></div>
-            <h4 className="title"><a href="shop-details.html">Box Full of Muscles</a></h4>
-            <span className="home-shop-price">$85.99</span>
-            <div className="home-shop-rating">
-              <Rating/>
-            </div>
+            <div className="shop-item-cat"><Category/></div>
+            <div className="title"><ProductName/></div>
+            <div className="home-shop-rating"><Rating/></div>
             <div className="shop-content-bottom">
                 <WishlistBtn/>
                 <AddCartBtn/>
             </div>
           </div>
         </div>
-      </div>
+      </div></div>
     );
   }
 }
