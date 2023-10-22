df556f9cdfe44423437c58d14c1f3e41198d95fa
ErangaKaushani <eranga.kaushani99@gmail.com>
2023-10-22 17:25:25 +0530

    Blog Modal and Blog Data Added

---
+++ Front-End/united-pharma-web-app/src/components/Blog/components/Blog/BlogModal.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function BlogModal(props) {
+  return (
+    //Bootstrap modal -- POP up Component
+    <div
+            className="modal fade"
+            id="myModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="myModal">
+                  {props.title} 
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  
+                  <div className="modal-body">
+                  <img src={props.image}/>
+                    {props.content}
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+  )
+}
