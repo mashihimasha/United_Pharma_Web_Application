7ca535ead7248c90f10d6ff3ca1ff9bd0dc9c2e1
Minoli Fonseka <mashihimasha@gmail.com>
2023-10-24 07:37:15 +0530

    Corrected registration success message not showing error

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,7 @@ app.post("/login", (req, res, next) => {
 
 //register
 app.post("/register", (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = r-eq.body;
   if(req.body !== null){
     User.findOne({ where: { email: email } }).then((user) => {
       if (user) {
