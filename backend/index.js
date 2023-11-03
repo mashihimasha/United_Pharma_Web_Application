258b60efa5a3af2be6804373fcdfbbb33854ba91
PeminduRayan <pemirayan@gmail.com>
2023-11-03 11:36:36 +0530

    Blog Routes modified

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,18 @@
 import express from 'express';
-import { createConnection } from 'mysql2';
-import {PORT} from './config/config.js';
+const app = express();
+import { urlencoded, json } from 'body-parser';
+import { config } from 'dotenv';
+import blogRoutes from './routes/blogRoutes';
+config();
 
-const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+const PORT = process.env.PORT || 3000;
 
-var database = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pemindu4132',
-    database: 'blog'
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+app.use('/api', blogRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-database.connect((err => {
-    if (err) throw err;
-    console.log('MySQL Connected');
-}));
-
-app.get('/inventory', (req, res) => {
-    let sql = 'SELECT * FROM inventory';
-    connection.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Inventory received');
-    });
-});
-
-
