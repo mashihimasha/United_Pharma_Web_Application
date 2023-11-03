79d460188f893b60d6bb54e280530a40cd91b93e
PeminduRayan <pemirayan@gmail.com>
2023-11-03 12:44:33 +0530

    Revert "backend modified"
    This reverts commit 2862e7786a043e3b5d7fdfd8dced4303f4bb4b77.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-import express, { json } from 'express';
+import express from 'express';
 const app = express();
-
+import { urlencoded, json } from 'body-parser';
 import { config } from 'dotenv';
-import blogRoutes from './Blog/routes/blogRoutes.js';
+import blogRoutes from './routes/blogRoutes';
 config();
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
+app.use(urlencoded({ extended: true }));
 app.use(json());
 
 app.use('/api', blogRoutes);
