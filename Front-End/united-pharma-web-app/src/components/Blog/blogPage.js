43d0c6a88423c943276c72aef7f433b74865ec12
ErangaKaushani <129719940+ErangaKaushani@users.noreply.github.com>
2023-11-10 18:24:43 +0530

    Header Added

--- Front-End/united-pharma-web-app/src/components/Blog/blogPage.js
+++ Front-End/united-pharma-web-app/src/components/Blog/blogPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Blog from './components/Blog/blog';
+import StickyHeader from '../common/Header';
 
 const  blogPage =  () => (
   <>
+  <StickyHeader/>
     <h1 className="text-center items-center mb-4 pt-3 ">United Pharma</h1>
     <Blog/>
   </>
