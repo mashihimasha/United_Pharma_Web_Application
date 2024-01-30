8ad344efea5a4d8bd32e459570b1c7b9451d34ab
ErangaKaushani <129719940+ErangaKaushani@users.noreply.github.com>
2024-01-30 11:15:01 +0530

    changes

--- Front-End/united-pharma-web-app/src/pages/Blog/blogPage.js
+++ Front-End/united-pharma-web-app/src/pages/Blog/blogPage.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Blog from './components/Blog/blog';
-import Footer from '../../components/common/Footer'
-import StickyHeader from '../../components/common/Header';
+import CreateBlogModal from './components/CreateBlogModal';
 
 const  blogPage =  () => (
   <>
-  <StickyHeader/>
     <h1 className="text-center items-center mb-4 pt-3 ">United Pharma</h1>
+    <CreateBlogModal/>
     <Blog/>
-    <Footer/>
   </>
 )
 
-export default blogPage
+export default blogPage;
