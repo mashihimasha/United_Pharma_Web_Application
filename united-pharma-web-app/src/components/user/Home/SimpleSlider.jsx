05462bc0380086c4c8ba194842f4ff1d87026e74
Minoli Fonseka <mashihimasha@gmail.com>
2024-02-11 02:34:45 +0530

    modified:   united-pharma-web-app/src/App.css 	new file:   united-pharma-web-app/src/components/assets/img/bg/masthead-bg1.jpg 	new file:   united-pharma-web-app/src/components/assets/img/bg/masthead-bg2.jpg 	new file:   united-pharma-web-app/src/components/assets/img/bg/masthead-bg3.jpg 	modified:   united-pharma-web-app/src/components/user/Home/SimpleSlider.jsx 	modified:   united-pharma-web-app/src/components/user/Home/VideoArea.jsx

--- united-pharma-web-app/src/components/user/Home/SimpleSlider.jsx
+++ united-pharma-web-app/src/components/user/Home/SimpleSlider.jsx
@@ -3,38 +3,30 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const SimpleSlider = () => {
+  const imageStyle = [
+   require('../../assets/img/bg/masthead-bg1.jpg'),
+   require('../../assets/img/bg/masthead-bg2.jpg'),
+   require('../../assets/img/bg/masthead-bg3.jpg'),
+  ];
+
   const settings = {
     dots: true,
     infinite: true,
-    speed: 100,
-    autoplaySpeed: 100,
+    speed: 4000,
+    autoplaySpeed: 4000,
     autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
 
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
+    <Slider {...settings} className="slider">
+      {imageStyle.map((imageStyle, index) => (
+        <div className="slider-image" key={index}>
+            <img src={imageStyle} alt="" />
+        </div>
+      ))}
     </Slider>
   );
 };
--- united-pharma-web-app/src/components/user/Home/VideoArea.jsx
+++ united-pharma-web-app/src/components/user/Home/VideoArea.jsx
@@ -6,11 +6,11 @@ const VideoArea = () => {
   return (
     <div className="video-area video-bg" data-background="assets/img/bg/video_bg.jpg" style={{ backgroundImage: 'url("../../assets/img/bg/video_bg.jpg")' }}>
       <div className="video-bg-overlay"></div>
-      <div className="container">
+      <div className="container-fluid g-0">
         <div className="row">
           <div className="col-12">
-            <div className="video-btn">
-              <SimpleSlider/>
+            <SimpleSlider/>
+            <div className="video-btn top-50">
               <Link to="/shoppingcart" className="ripple-white  d-flex align-items-center"><h6 className='fw-bolder'>Start Shopping</h6></Link>
             </div>
           </div>
