e89c164ec400a9c444196273fd51cd8ba4dad031
ErangaKaushani <129719940+ErangaKaushani@users.noreply.github.com>
2024-02-11 22:05:37 +0530

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,19 +1,10 @@
 import mysql from "mysql2";
-import dotenv from "dotenv";
-dotenv.config();
 
-const pool = mysql.createPool({
-    host : process.env.HOST,
-    user : process.env.USER,
-    password : process.env.PW,
-    database : process.env.DB
-}).promise();
+const connection = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Pemindu4132",
+    database: "blog",
+});
 
-
-async function getData(){
-    const [data] = await pool.query("SELECT * FROM blog") ;    
-    return data;
-}
-
-const dat = await getData();
-console.log(dat);
+export default connection.promise();
